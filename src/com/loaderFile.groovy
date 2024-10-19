884cc99302bfb6aa0f5c3b2e868f1f4d904cb021
MohnishBhonde <90518467+MohnishBhonde@users.noreply.github.com>
2024-10-20 03:22:32 +0530

    Update loaderFile.groovy

--- src/com/loaderFile.groovy
+++ src/com/loaderFile.groovy
@@ -3,18 +3,70 @@ def localDir = '/var/lib/jenkins' // Set your local directory path here
 // Check if the directory exists
 def repoExists = new File(localDir).exists()
 
-def runPipeline() {
-    if (repoExists) {
-        println "Repository exists at ${localDir}. Performing a git pull..."
+//def runPipeline() {
+    //if (repoExists) {
+        //println "Repository exists at ${localDir}. Performing a git pull..."
         // Execute git pull
-        def processPull = "git -C ${localDir} pull".execute()
-        processPull.waitFor()
-        println processPull.text
-    } else {
-        println "Repository does not exist at ${localDir}. Cloning from ${repoUrl}..."
+        //def processPull = "git -C ${localDir} pull".execute()
+        //processPull.waitFor()
+        //println processPull.text
+    //} //else {
+        //println "Repository does not exist at ${localDir}. Cloning from ${repoUrl}..."
         // Execute git clone
-        def processClone = "git clone ${repoUrl} ${localDir}".execute()
-        processClone.waitFor()
-        println processClone.text
+        //def processClone = "git clone ${repoUrl} ${localDir}".execute()
+        //processClone.waitFor()
+        //println processClone.text
+    //}
+//}
+
+
+def call() {
+    
+  pipeline {
+     agent any
+
+    tools {
+        maven "maven1" // You need to add a maven with name "3.6.0" in the Global Tools Configuration page
     }
-}
+
+    stages {
+        stage("Build") {
+            steps {
+                
+              dir('/var/lib/jenkins'){
+                  if (repoExists){
+                      // Performing git pull
+                      def processPull = "git -C ${localDir} pull".execute()
+                      processPull.waitFor()
+                      println processPull.text
+                  }
+                  else{
+                  //performing git clone
+                      def processClone = "git clone ${repoUrl} ${localDir}".execute()
+                      processClone.waitFor()
+                      println processClone.text
+                  }
+               // sh "git pull https://github.com/MohnishBhonde/SNSImplemen"
+                
+               
+                
+              }
+                
+            }
+         }
+        stage("mvn install"){
+            steps{
+                 dir('/var/lib/jenkins/workspace/shared_jfrog_m/SNSImplemen'){
+                  sh "mvn -version"
+                  sh "mvn clean install"
+                 }
+            }
+        }
+        
+            stage("mvn package"){
+                steps{
+                    dir('/var/lib/jenkins/workspace/shared_jfrog_m/SNSImplemen'){
+                    sh "mvn package"
+                    }
+                }
+            }
